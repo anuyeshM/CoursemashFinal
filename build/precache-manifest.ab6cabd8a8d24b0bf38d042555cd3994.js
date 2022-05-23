ac4731da1fae3ab1a50b8da4903a5f81282fbefc
Anuyesh Mishra <95938949+anuyeshM@users.noreply.github.com>
2022-05-23 15:15:38 +0530

    Add files via upload

---
+++ build/precache-manifest.ab6cabd8a8d24b0bf38d042555cd3994.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = [
+  {
+    "revision": "54ffffd30d03a27f67ea",
+    "url": "/static/css/main.865f2852.chunk.css"
+  },
+  {
+    "revision": "54ffffd30d03a27f67ea",
+    "url": "/static/js/main.54ffffd3.chunk.js"
+  },
+  {
+    "revision": "62b29575e2b919396439",
+    "url": "/static/js/1.62b29575.chunk.js"
+  },
+  {
+    "revision": "954c16dd338d68044ab8",
+    "url": "/static/js/2.954c16dd.chunk.js"
+  },
+  {
+    "revision": "88acd06e1faf55547e53",
+    "url": "/static/js/runtime~main.88acd06e.js"
+  },
+  {
+    "revision": "2524428719ff5baa692a1c3631dd6d21",
+    "url": "/static/media/hero-b.25244287.png"
+  },
+  {
+    "revision": "1793da33fb666ee72dd63346d9d0306c",
+    "url": "/static/media/outcomes.1793da33.png"
+  },
+  {
+    "revision": "655e7cb9efa2433892a5e96323e11e08",
+    "url": "/static/media/Image-Next-Step.655e7cb9.png"
+  },
+  {
+    "revision": "35c3460d6d08cc093d79a5c26535d507",
+    "url": "/static/media/contact.35c3460d.png"
+  },
+  {
+    "revision": "cf4d7e4005241d7cebc2c4f6c08d042b",
+    "url": "/static/media/pngwing.com.cf4d7e40.png"
+  },
+  {
+    "revision": "74b7ac1584bd9550992b08301a50a04e",
+    "url": "/static/media/certificate (1).74b7ac15.png"
+  },
+  {
+    "revision": "21d630ce2e28c5d8fec80ce0881106dc",
+    "url": "/index.html"
+  }
+];
